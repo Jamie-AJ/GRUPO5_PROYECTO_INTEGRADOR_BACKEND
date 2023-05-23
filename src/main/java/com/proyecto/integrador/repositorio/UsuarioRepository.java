86383d1e32e9624031593b1b33529d40d495bf7a
Bruno Daniel Rios Cosser <bruno_rioscosser18_2314@hotmail.com>
2023-05-22 22:24:51 -0500

    Cambio al metodo de retiro y deposito

--- src/main/java/com/proyecto/integrador/repositorio/UsuarioRepository.java
+++ src/main/java/com/proyecto/integrador/repositorio/UsuarioRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.proyecto.integrador.entidades.Empresa;
 import com.proyecto.integrador.entidades.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
