6d7bf2a686f9191f32d57b7974a9c63737778bda
ValeryHi <valeriahilario28@gmail.com>
2023-06-20 00:25:29 -0500

    listado de oportunidades

--- src/main/java/com/proyecto/integrador/servicios/OportunidadUsuarioService.java
+++ src/main/java/com/proyecto/integrador/servicios/OportunidadUsuarioService.java
@@ -10,5 +10,9 @@ public interface OportunidadUsuarioService {
 	public List<OportunidadUsuario> listaOportunidadUsuario();
 	
 	public OportunidadUsuario RegistrarActualizarOportunidad(OportunidadUsuario opusu );
+	
+	public abstract List<OportunidadUsuario> listarUsuarioxOpo(int idOpo); 
+	
+	public abstract List<OportunidadUsuario> listarUsuarioOportunidadxID(long idUsu); 
 
 }
